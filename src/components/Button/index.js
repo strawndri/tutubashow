afc63aba6e61d7fd8ce0be70d66498160d7d54d7
Andrieli <andrieliluci@gmail.com>
2020-07-30 10:40:00 -0300

    commit - aula 4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 const Button = styled.button`
  color: var(--white);
     border: 1px solid var(--white);
+    background: solid var(--black);
     box-sizing: border-box;
     cursor: pointer;
-    padding: 16px 24px;
+    padding: 10px 20px;
     font-style: normal;
     font-weight: bold;
     font-size: 16px;
     outline: none;
     border-radius: 5px;
+    border-width: 5px;
     text-decoration: none;
     display: inline-block;
     transition: opacity .3s;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -2,90 +2,92 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
+import Button from '../../../components/Button'
 
 function CadastroCategoria() {
-const valoresIniciais ={
-  nome: '',
-  descricao: '',
-  cor: '',
-}
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
 
-const [categorias, setCategorias] = useState([]);
-const [values, setValues] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
   function setValue(chave, valor) {
     setValues({
       ...values,
-      [chave]: valor, //nome: 'valor'
-    })
+      [chave]: valor, // nome: 'valor'
+    });
   }
 
-  function handleChange(infosDoEvento){
+  function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     );
-    }  
+  }
 
   return (
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
-      
-      <form onSubmit={function handleSubmit(infosDoEvento){
-      infosDoEvento.preventDefault()  
-      setCategorias([
-        ...categorias,
-        values
-      ]);
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-          setValues(valoresIniciais)
-     }}>
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
 
-<FormField 
-    label="Nome da Categoria"
-    type="text"
-    name="nome"
-    value={values.nome}
-    onChange={handleChange}
-/>
+        setValues(valoresIniciais);
+      }}
+      >
 
-<FormField 
-      label="Descrição"
-      type="textarea"
-      name="descricao"
-      value={values.descricao}
-      onChange={handleChange}
-/>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-      <FormField 
-      label="Cor"
-      type="color"
-      name="cor"
-      value={values.cor}
-      onChange={handleChange}
-/>
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
 
-        <button>
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
           Cadastrar
-        </button>
+        </Button>
       </form>
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-        }
-        )}
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria}${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">
         Ir para home
-        </Link>
+      </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;

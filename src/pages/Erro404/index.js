0030be678eb7614d7284a7dd0f2c95f321354e71
Andrieli <andrielilluci@gmail.com>
2020-09-14 14:06:08 -0300

    * problemas na pág Erro404

--- src/pages/Erro404/index.js
+++ src/pages/Erro404/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PageDefault from '../../components/PageDefault';
 import { Link } from 'react-router-dom'
-import  './erro404.css'
 
 function Erro404() {
     return (
